9f4b266a663d779ec6e8e5b5c0aab69ffc87c6f9
Muthu Kannan Subramanian <you@example.com>
2017-09-25 02:08:21 -0500

    First Commit

---
+++ latlong.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 06 13:47:11 2017
+
+@author: ArunKumar
+"""
+from bs4 import BeautifulSoup
+#import re
+#import os
+#import urllib2
+
+
+infile = r"C:\Users\ArunKumar\Desktop\Project\project 3\countries.htm"
+outfile = open('latlong.csv','w')
+
+
+soup1 = BeautifulSoup(open(infile).read())
+
+
+
+latlon = soup1.find('div',{"itemprop":"articleBody"})
+cnt = []
+for row in latlon.find_all('tr'):
+    col = row.find_all('td')
+    print col
+    cnt.append(col)
+    
+    
+    if len(col) != 0:
+        
+        lat = col[1].get_text()
+        lon = col[2].get_text()
+        country = col[3].get_text()
+        
+    
+        outfile.write(country.encode('utf8')+",")
+        outfile.write(lat.encode('utf8')+",")
+        outfile.write(lon.encode('utf8')+"\n")
+ 
+outfile.close()
